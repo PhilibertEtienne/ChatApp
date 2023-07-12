974f4c558d7a48e23f03913c89f164b8e76b8297
philibertEtienne <philibert.etienne4@orange.fr>
2023-07-12 09:44:45 +0200

    fixredirect

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,71 +1,79 @@
-import React, { useState } from 'react'
-import { Row, Col, Form, Button } from 'react-bootstrap'
-import { gql, useLazyQuery, useQuery } from '@apollo/client'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import { gql, useLazyQuery } from '@apollo/client';
+import { Link, useNavigate } from 'react-router-dom';  // Import useNavigate
 
 const LOGIN_USER = gql`
-  query login(
-    $username: String!
-    $password: String!
-  ) {
-    login(
-      username: $username
-      password: $password
-    ) {
+  query login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       username
       email
       createdAt
       token
     }
   }
-`
+`;
 
-export default function Register(props) {
+export default function Login(props) {
   const [variables, setVariables] = useState({
     username: '',
     password: '',
-  })
-  const [errors, setErrors] = useState({})
+  });
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();  // Use the useNavigate hook
 
   const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
-    onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
+    onError: (err) => setErrors(err.graphQLErrors[0]?.extensions.errors),
     onCompleted: (data) => {
-    localStorage.setItem('token', data.login.token)
-    props.history.push('/')
+      localStorage.setItem('token', data.login.token);
+      navigate('/');  // Use the navigate function to redirect
     }
-  })
+  });
 
   const submitLoginForm = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    loginUser({ variables })
-  }
-
+    loginUser({ variables });
+  };
 
   return (
-    <Row className='bg-white py-5 justify-content-center'>
-    <Col sm={8} md={6} lg={4}>
-    <h1 className="text-center">Login</h1>
-      <Form onSubmit={submitLoginForm}>
-        <Form.Group>
-          <Form.Label className={errors.username && 'text-danger'}>
+    <Row className="bg-white py-5 justify-content-center">
+      <Col sm={8} md={6} lg={4}>
+        <h1 className="text-center">Login</h1>
+        <Form onSubmit={submitLoginForm}>
+          <Form.Group>
+            <Form.Label className={errors.username && 'text-danger'}>
               {errors.username ?? 'Username'}
-          </Form.Label>
-          <Form.Control type="text" placeholder="Enter username" value={variables.username} className={errors.username && 'is-invalid'} onChange={e => setVariables({...variables,username: e.target.value})}/>
-        </Form.Group>
-        <Form.Group >
-          <Form.Label className={errors.password && 'text-danger'}>
-            {errors.password ?? 'Password'}
-          </Form.Label>
-          <Form.Control type="password" placeholder="Enter password" value={variables.password} className={errors.password && 'is-invalid'}  onChange={e => setVariables({...variables,password: e.target.value})}/>
-        </Form.Group>
-        <Button variant="success" type="submit" disabled={loading} className='my-3'>
-          { loading ? 'loading..' : 'Login'}
-        </Button>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={variables.username}
+              className={errors.username && 'is-invalid'}
+              onChange={(e) => setVariables({ ...variables, username: e.target.value })}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className={errors.password && 'text-danger'}>
+              {errors.password ?? 'Password'}
+            </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={variables.password}
+              className={errors.password && 'is-invalid'}
+              onChange={(e) => setVariables({ ...variables, password: e.target.value })}
+            />
+          </Form.Group>
+          <Button variant="success" type="submit" disabled={loading} className="my-3">
+            {loading ? 'loading..' : 'Login'}
+          </Button>
           <br></br>
-          <small>Don't have an account ? <Link to="/register"> Register</Link> </small>
-      </Form>
-    </Col>
-  </Row>
-  )
+          <small>
+            Don't have an account? <Link to="/register">Register</Link>
+          </small>
+        </Form>
+      </Col>
+    </Row>
+  );
 }
